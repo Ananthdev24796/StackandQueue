07e269b5b1948afb2363a4d97ad94031f8ae8f56
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-23 16:37:11 +0530

    Added:Stack and queue

---
+++ src/com/MyStackTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com;
+
+/**
+ * @author Iam_A
+ *
+ */
+public class MyStackTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
